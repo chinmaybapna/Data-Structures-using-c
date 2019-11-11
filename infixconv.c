2dc995a6948a08ab3c5b6e3f87859ac702a226e5
Chinmay Bapna <chinmaybapna111.cb@gmail.com>
2019-11-11 21:09:03 +0530

    Update infixconv.c

--- infixconv.c
+++ infixconv.c
@@ -1,116 +1,101 @@
 #include<stdio.h>
-
-#define size 30
-char infix[size],postfix[size],ch,top=-1,s[size];
-
+#define size 10
+char s[size], infix[size], postfix[size], temp, ch;
+int top=-1, i=0, j=0;
 void push(char ch)
 {
-	s[++top]=ch;	
+    s[++top] = ch;
 }
+
 char pop()
 {
-	return(s[top--]);
-}
-
-int precedence(char ch)
-{
-	switch(ch)
-	{
-		case '^':return(4);break;
-		case '/':
-		case '*':return(3);break;
-		case '+':
-		case '-':return(2);break;
-		default :return(1);break;
-	}
-
+    return(s[top--]);
 }
 
 int isoperator(char ch)
 {
-	if(ch=='^'||ch=='/'||ch=='*'||ch=='+'||ch=='-')
-		return(1);
-	else
-		return(0);
+    if(ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^')   
+        return(1);
+    else
+        return(0);
+    
 }
 
-void main()
+int precedence(char ch)
 {
-	int i=0,j=0,temp;	
-
-	printf("enter the infix equation:\n");
-	gets(infix);
-	
-	while(infix[i]!='\0')
-	{
-		ch = infix[i];
-		
-		if(isalpha(ch))
-		{
-			postfix[j]=ch;
-			j++;
-		}
-
-		else
-			if(ch=='(')
-			{
-				push(ch);
-			}
-		else
-			if(isoperator(ch))
-			{
-				if(ch!='^')
-					while(top!=-1 && precedence(s[top]>=precedence(ch)))
-					{
-						postfix[j]=pop();
-						j++;
-					}
-				push(ch);
-			}
-		else 
-			if(ch==')')
-			{
-				while(s[top]!='(')
-				{
-					postfix[j]=pop();
-					j++;
-				}
-				
-				temp=pop();
-			}
-		else
-		{
-			printf("invalid character\n");
-		}
-		i++;
-	}
-
-		while(top>-1)
-		{
-			postfix[j]=pop();
-			j++;
-		}
-			
-	postfix[j]='\0';
-	
-	printf("the postfix expression is:\n");
-		printf("%s\n",postfix);
+    switch(ch)
+    {
+        case '^': return(4); break;
+        case '*':
+        case '/': return(3); break;
+        case '+': 
+        case '-': return(2); break;
+        default: return(1);break;
+    }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ void main()
+ {
+     printf("enter the infix epxression\n");
+     gets(infix);
+     while(infix[i] != '\0')
+     {
+        ch = infix[i];
+         
+        if(isalnum(ch))
+        {
+            postfix[j] = ch;
+            j++;
+        }
+        
+        else if(ch == '(')
+        {
+            push(ch);
+        }
+        
+        else if(isoperator(ch))
+        {
+            if(ch != '^')
+            {
+                while(top != -1 && precedence(s[top]) > precedence(ch))
+                {
+                    postfix[j] = pop();
+                    j++;
+                }
+            }
+            
+            push(ch);
+        }
+        
+        else if(ch == ')')
+        {
+            while(s[top] != '(')
+            {
+                postfix[j] = pop();
+                j++;
+            }
+            temp = pop();
+        }
+        
+        else
+        {
+            printf("invalid character\n");
+        }
+        
+        i++;
+     }
+     
+     while(top != -1)
+     {
+         postfix[j] = pop();
+         j++;
+     }
+     postfix[j] = '\0';
+     
+    printf("the postfix expression is:\n");
+		printf("%s\n",postfix);
+ }
+ 
+ 
+ 
+ 
