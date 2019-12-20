45da621fb70b7cf72acf1eb41fe8a0c1c2abe0f9
Chinmay Bapna <chinmaybapna111.cb@gmail.com>
2019-12-20 16:42:11 +0530

    Create FibonacciRecursion.java

---
+++ sem3/OOPS-java/FibonacciRecursion.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Fibo {
+    static int fiboRec(int n)
+    {
+        if(n < 2)
+        {
+            return n;
+        }
+        else
+        {
+            return fiboRec(n-1) + fiboRec(n-2);
+        }
+    }
+}
+
+class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size:");
+        int size = sc.nextInt();
+        for(int i=0; i<size; i++)
+        {
+            System.out.print(Fibo.fiboRec(i) + " ");
+        }
+    }
+}
