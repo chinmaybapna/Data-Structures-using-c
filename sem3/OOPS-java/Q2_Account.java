996e3963362e8f29f64f6e0af0e7595e0b4838ee
Chinmay Bapna <chinmaybapna111.cb@gmail.com>
2019-12-22 23:18:04 +0530

    Update and rename AccountActions.java to Q2_Account.java

--- sem3/OOPS-java/Q2_Account.java
+++ sem3/OOPS-java/Q2_Account.java
@@ -48,7 +48,7 @@ class Account {
     }
 }
 
-class AccountActions {
+class Q2_Account {
     public static void main (String[] args) {
         int ch;
         Scanner sc = new Scanner(System.in);
